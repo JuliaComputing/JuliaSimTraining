af2e2ff4b9f077ed076624956ebd9ea2247f0a8e
Jacob Vaverka <jvaverka@pm.me>
2023-03-21 22:14:44 +0000

    docs: run batch jobs on JuliaHub

--- 01_JuliaHub/02_Batch_Job.jl
+++ 01_JuliaHub/02_Batch_Job.jl
@@ -1,84 +1,57 @@
-# # Modeling Toolkit Example
-# Build the DC Motor System using the ModelingToolkit Standard Library.
-using ModelingToolkit, DifferentialEquations, Plots, ControlSystemsBase
-using ModelingToolkitStandardLibrary,
-    ModelingToolkitStandardLibrary.Electrical,
-    ModelingToolkitStandardLibrary.Mechanical.Rotational,
-    ModelingToolkitStandardLibrary.Blocks
-@variables t
-# R = 0.5 # [Ohm] armature resistance
-# L = 4.5e-3 # [H] armature inductance
-# k = 0.5 # [N.m/A] motor constant
-# J = 0.02 # [kg.m²] inertia
-# f = 0.01 # [N.m.s/rad] friction factor
-function Motor(; name, R=0.5, L=4.5e-3, k=0.5, J=0.02, f=0.01)
-    @named ground = Ground()
-    @named source = Voltage()
-    @named R1 = Resistor(; R=R)
-    @named L1 = Inductor(; L=L)
-    @named emf = EMF(; k=k)
-    @named fixed = Fixed()
-    @named load = Torque(; use_support=false)
-    @named inertia = Inertia(; J=J)
-    @named friction = Damper(; d=f)
+# # Running Batch Jobs on JuliaHub
+# The JuliaHub extension makes it easy to scale your simulations by leveraging cloud compute.
+# For instance, say we want to run our model over a longer timespan but do not want to wait for the simulation to complete.
+# We can kickoff a batch job on JuliaHub to execute the simulation, and we can view the logs for the job at a later date.
+# To get started, navigate to the JuliaHub extension by clicking the JuliaHub logo on the left-most sidebar
+# (or by using the command palette, `CTRL + SHIFT + P`, and executing the `View: Show JuliaHub` command).
+#
+# ## JuliaHub Extension Overview
+# The basic sections of the JuliaHub extension can be summarized as follows:
+# - **Julia script**: the code to execute
+# - **Bundle directory**: the directory containing any and all files necessary for successful execution
+# - **Compute configuration**: specifications for the compute environment to use for execution
+#   - Should the run occur on a _single-process_ or _distributed_?
+#   - Should the run occur on _CPUs_ or _GPUs_?
+#   - How powerful should each machine be?
+# - **Number of Nodes**: the total number of machines to use for execution
+# - **Limit**: the maximum time or cost limit should the code run longer than expected
+# - **Image options**: the environment to use for execution (`Default` is a standard Julia environment while `JuliaSim` contains all proprietary JuliaSim packages)
+# - **Job name**: a descriptive name to identify the job
+# - **Inputs**: any input values expected by the _Julia script_.
+#
+# ## JuliaHub Example Run
+# To run this example, follow the steps below:
+# 1. Open this file (`JuliaSimTraining/01_JuliaHub/02_Batch_Job.jl`) in the editor
+# 1. Navigate to the JuliaHub extension
+# 1. **Julia script**: click _Use current file_
+# 1. **Bundle**: do nothing
+# 1. **Compute configuration**: Start a `distributed` `CPU` job with `8` vCPUs per node and `4` GB of memory per vCPU with one Julia process for each `vCPU`.
+# 1. **Number of Nodes**: set to `1`
+# 1. **Limit**: do nothing
+# 1. **Image options**: select `JuliaSim`
+# 1. **Job name**: enter `<your name> first batch job`
+# 1. **Inputs**: do nothing
+# 1. Click **Start Job**!
 
-    connections = [
-        connect(fixed.flange, emf.support, friction.flange_b)
-        connect(emf.flange, friction.flange_a, inertia.flange_a)
-        connect(inertia.flange_b, load.flange)
-        connect(source.p, R1.p)
-        connect(R1.n, L1.p)
-        connect(L1.n, emf.p)
-        connect(emf.n, source.n, ground.g)
-    ]
-    subcomps = [ground, source, R1, L1, emf, fixed, load, inertia, friction]
-    @named model = ODESystem(connections, t)
-    return compose(model, subcomps)
-end
+using ModelingToolkit, Plots, DifferentialEquations
 
-pi_k = 1.1
-pi_T = 0.05
-@named motor = Motor();
-tau_L_step = -0.3 # [N.m] amplitude of the load torque step
-@named ref = Blocks.Step(; height=1, start_time=0)
-@named pi_controller = Blocks.LimPI(; k=pi_k, T=pi_T, u_max=10, Ta=0.035)
-@named feedback = Blocks.Feedback()
-@named load_step = Blocks.Step(; height=tau_L_step, start_time=3)
-@named speed_sensor = SpeedSensor()
+@variables t x(t) y(t)
+@parameters α β δ γ
+D = Differential(t)
+eqs = [
+    D(x) ~ α * x - β * x * y
+    D(y) ~ δ * x * y - γ * y
+];
 
-connections = [
-    connect(motor.load.flange, speed_sensor.flange)
-    connect(ref.output, feedback.input1)
-    connect(speed_sensor.w, :y, feedback.input2)
-    connect(load_step.output, motor.load.tau)
-    connect(feedback.output, pi_controller.err_input)
-    connect(pi_controller.ctr_output, :u, motor.source.V)
-]
+@named model = ODESystem(eqs, t);
 
-subcomps = [motor, ref, pi_controller, feedback, load_step, speed_sensor]
-@named model = ODESystem(connections, t)
-model = compose(model, subcomps)
-
-# # Extend Problem Timespan
-# Launch batch job
-
-sys = structural_simplify(model)
-prob = ODEProblem(sys, [], (0, 6.0))
-sol = solve(prob, Rodas4())
-
-p1 = Plots.plot(
-    sol.t,
-    sol[motor.inertia.w];
-    ylabel="Angular Vel. in rad/s",
-    label="Measurement",
-    title="DC Motor with Speed Controller",
+prob = ODEProblem(
+    model, [x => 0.9, y => 1.8], (0, 3600.0), [α => 2 / 3, β => 4 / 3, γ => 1, δ => 1]
 )
-Plots.plot!(sol.t, sol[ref.output.u]; label="Reference")
-p2 = Plots.plot(sol.t, sol[motor.load.tau.u]; ylabel="Disturbance in Nm", label="")
-Plots.plot(p1, p2; layout=(2, 1))
 
-mat, simplified_sys = get_sensitivity(model, :y);
-S = ss(mat...);
-bplot = bodeplot(S; plotphase=false)
-nplot = nyquistplot(-ss(get_looptransfer(model, :u)[1]...))
-Plots.plot(p1, p2, bplot, nplot; layout=(2, 2))
+sol = solve(prob)
+plot(sol)
+
+# ## Next Steps
+# You have the basic knowledge required to run batch jobs on JuliaHub.
+# In the next exercise (`JuliaSimTraining/01_JuliaHub/03_Batch_Job_Logging.jl`), you'll learn about the rich logging features of JuliaHub.
