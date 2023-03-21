32dc3bb369320ffede0c07d354f76de3474a3b10
Jacob Vaverka <jvaverka@pm.me>
2023-03-21 22:14:06 +0000

    docs: get up and running on JuliaHub

--- 01_JuliaHub/01_Interactive_Development.jl
+++ 01_JuliaHub/01_Interactive_Development.jl
@@ -1,87 +1,54 @@
-# # Modeling Toolkit Example
-# Build the DC Motor System using the ModelingToolkit Standard Library.
-using ModelingToolkit, DifferentialEquations, Plots, ControlSystemsBase
-using ModelingToolkitStandardLibrary, ModelingToolkitStandardLibrary.Electrical, ModelingToolkitStandardLibrary.Mechanical.Rotational, ModelingToolkitStandardLibrary.Blocks
-@variables t
-# R = 0.5 # [Ohm] armature resistance
-# L = 4.5e-3 # [H] armature inductance
-# k = 0.5 # [N.m/A] motor constant
-# J = 0.02 # [kg.m²] inertia
-# f = 0.01 # [N.m.s/rad] friction factor
-function Motor(; name, R = 0.5, L = 4.5e-3, k = 0.5, J = 0.02, f = 0.01)
-    @named ground = Ground()
-    @named source = Voltage()
-    @named R1 = Resistor(R = R)
-    @named L1 = Inductor(L = L)
-    @named emf = EMF(k = k)
-    @named fixed = Fixed()
-    @named load = Torque(use_support = false)
-    @named inertia = Inertia(J = J)
-    @named friction = Damper(d = f)
+# # Interactive Development with JuliaSim
+# JuliaSim IDE is the interactive development environment for working with JuliaSim.
+# To get started, login to <juliahub.com> and click **launch** on JuliaSim IDE (if JuliaSim IDE does not appear on the home page then go to <juliahub.com/ui/Applications>).
+# JuliaHub will spin up a JuliaSim IDE for you. Once it is ready simply click **connect**.
+#
+# ## What is JuliaSim IDE?
+# In essence, JuliaSim IDE is VS Code in a web browser plus a precompiled environment suited for simulation and modeling.
+# This environment contains common packages for modeling such as `ModelingToolkit.jl`, `Plots.jl` and `DifferentialEquations.jl`.
+# All JuliaSim packages are also available in the environment and can be accessed like any other package by the `using` keyword.
+#
+# ## Loading Packages
+# This example requires three packages; load them by executing the line below.
 
-    connections = [connect(fixed.flange, emf.support, friction.flange_b)
-                   connect(emf.flange, friction.flange_a, inertia.flange_a)
-                   connect(inertia.flange_b, load.flange)
-                   connect(source.p, R1.p)
-                   connect(R1.n, L1.p)
-                   connect(L1.n, emf.p)
-                   connect(emf.n, source.n, ground.g)]
-    subcomps = [
-        ground,
-        source,
-        R1,
-        L1,
-        emf,
-        fixed,
-        load,
-        inertia,
-        friction,
-    ]
-    @named model = ODESystem(connections, t)
-    compose(model, subcomps)
-end
+using ModelingToolkit, Plots, DifferentialEquations
 
-pi_k = 1.1
-pi_T = 0.05
-@named motor = Motor();
-tau_L_step = -0.3 # [N.m] amplitude of the load torque step
-@named ref = Blocks.Step(height = 1, start_time = 0)
-@named pi_controller = Blocks.LimPI(k = pi_k, T = pi_T, u_max = 10, Ta = 0.035)
-@named feedback = Blocks.Feedback()
-@named load_step = Blocks.Step(height = tau_L_step, start_time = 3)
-@named speed_sensor = SpeedSensor()
+# ## Executing Code
+# - `CTRL + ENTER`: sends current line to REPL
+# - `SHIFT + ENTER`: sends current line to REPL and moves to the next line
+# Execute the lines below to declare the variables, parameters and differential for this example.
 
-connections = [
-            connect(motor.load.flange, speed_sensor.flange)
-            connect(ref.output, feedback.input1)
-            connect(speed_sensor.w, :y, feedback.input2)
-            connect(load_step.output, motor.load.tau)
-            connect(feedback.output, pi_controller.err_input)
-            connect(pi_controller.ctr_output, :u, motor.source.V)]
+@variables t x(t) y(t)
+@parameters α β δ γ
+D = Differential(t)
 
-subcomps = [
-    motor,
-    ref,
-    pi_controller,
-    feedback,
-    load_step,
-    speed_sensor,
-]
-@named model = ODESystem(connections, t)
-model = compose(model, subcomps)
+# ## Executing Code Cells
+# The Julia VS Code extension allows us to group lines of code together into _cells_.
+# When working interactively, code cells conveniently allow users to execute related lines of code together.
+# Use any one of the following to delineate code cells:
+# - `#-`
+# - `##`
+# - `#%%`
+# `ALT + ENTER` executes the current code cell.
+# `SHIFT + ALT + ENTER` executes the current code cell and moves to the next cell.
+# Use the code cells below for practice.
 
-sys = structural_simplify(model)
-prob = ODEProblem(sys, [], (0, 6.0))
-sol = solve(prob, Rodas4())
+#-
+eqs = [
+    D(x) ~ α * x - β * x * y
+    D(y) ~ δ * x * y - γ * y
+];
+@named model = ODESystem(eqs, t);
 
-p1 = Plots.plot(sol.t, sol[motor.inertia.w], ylabel = "Angular Vel. in rad/s",
-                label = "Measurement", title = "DC Motor with Speed Controller")
-Plots.plot!(sol.t, sol[ref.output.u], label = "Reference")
-p2 = Plots.plot(sol.t, sol[motor.load.tau.u], ylabel = "Disturbance in Nm", label = "")
-Plots.plot(p1, p2, layout = (2, 1))
+prob = ODEProblem(
+    model, [x => 0.9, y => 1.8], (0, 20.0), [α => 2 / 3, β => 4 / 3, γ => 1, δ => 1]
+)
+#-
+sol = solve(prob)
+#-
+plot(sol)
 
-mat, simplified_sys = get_sensitivity(model, :y);
-S = ss(mat...);
-bplot = bodeplot(S, plotphase=false)
-nplot = nyquistplot(-ss(get_looptransfer(model, :u)[1]...))
-Plots.plot(p1, p2, bplot, nplot, layout = (2, 2))
+# ## Next Steps
+# You have now performed some interactive work in JuliaSim IDE.
+# The Julia VS Code extension offers many more [keybindings & commands](https://www.julia-vscode.org/docs/stable/userguide/keybindings/) that can improve your productivity in JuliaSim IDE.
+# In the next exercise (`JuliaSimTraining/01_JuliaHub/02_Batch_Job.jl`), you'll learn how to run batch jobs on JuliaHub.
